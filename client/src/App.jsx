36f11fcc38ad889068c98059936c103519a8cdf5
MuhammadUmar7195 <mmuhammadumar869@gmail.com>
2025-06-03 21:53:20 +0500

    React with shadcn setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+import NavBar from "./components/shared/Navbar";
+
+const App = () => {
+  return (
+    <div className="flex p-5">
+      <NavBar/>
+    </div>
+  );
+};
+
+export default App;
